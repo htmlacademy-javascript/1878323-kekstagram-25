bf668fcd312eb6c2746d5768987668252b963b01
koteech <nosenkoaa@mbsi.bz>
2022-03-02 21:33:15 +0300

    4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -25,6 +25,18 @@ const DESCRIPTION = [
 ];
 
 const MAX_POST_ID = 25;
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const MIN_COMMENTS_ID = 1;
+const MAX_COMMENTS_ID = 5;
+const MIN_LIKES_ID = 15;
+const MAX_LIKES_ID = 200;
+
+let createIdGenerator = () => {
+  let lastGeneratedId = 0;
+  return () => lastGeneratedId++;
+};
+
 let commentId = 1;
 let postId = 1;
 
@@ -43,19 +55,19 @@ const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0,
 
 const createPostsComments = () => ({
   id: commentId++,
-  avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
-  message: getRandomArrayElement (COMMENTS),
-  name: getRandomArrayElement (NAMES),
+  avatar: `img/avatar-${getRandomPositiveInteger(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`,
+  message: getRandomArrayElement(COMMENTS),
+  name: getRandomArrayElement(NAMES),
 });
 
 const createPosts = () => ({
   id: postId,
   url: `photos/${postId++}.jpg`,
-  description: getRandomArrayElement (DESCRIPTION),
-  likes: getRandomPositiveInteger(15,200),
-  comments: Array.from({length: getRandomPositiveInteger(1,5)},createPostsComments),
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomPositiveInteger(MIN_LIKES_ID, MAX_LIKES_ID),
+  comments: Array.from({length: getRandomPositiveInteger(MIN_COMMENTS_ID, MAX_COMMENTS_ID)}, createPostsComments),
 });
 
-const similarPosts = Array.from({length: MAX_POST_ID},createPosts);
+const similarPosts = () => Array.from({length: MAX_POST_ID}, createPosts);
 
-console.log(similarPosts);
+console.log(similarPosts());

c3fe95c28af925cdb70d5bd7b5cad0f27abefb69
koteech <nosenkoaa@mbsi.bz>
2022-03-11 02:06:17 +0300

    7.10. Отрисуй меня полностью (часть 2)

---
+++ js/picture-small.js
@@ -0,0 +1,17 @@
+import {createPosts} from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const newItemPicture = pictureTemplate.querySelector('.picture');
+const pictureList = document.querySelector('.pictures');
+const itemPhotosDescriptions = createPosts();
+const pictureListFragment = document.createDocumentFragment();
+
+itemPhotosDescriptions.forEach((picture) => {
+  const copyItemPicture = newItemPicture.cloneNode(true);
+  copyItemPicture.querySelector('img').src = picture.url;
+  copyItemPicture.querySelector('.picture__likes').textContent = picture.likes;
+  copyItemPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureListFragment.append(copyItemPicture);
+});
+
+pictureList.append(pictureListFragment);

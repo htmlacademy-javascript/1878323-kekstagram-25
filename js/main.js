1bca297f955458d8bdc5afbd7e143582ac2b090d
koteech <nosenkoaa@mbsi.bz>
2022-03-11 11:23:29 +0300

    7.10. Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-import {createPictureListArray} from './picture.js';
+import {createItemPicture} from './picture.js';
+import {createPosts} from './data.js';
 const pictureList = document.querySelector('.pictures');
+const itemPictureDescriptions = createPosts();
+const pictureListFragment = document.createDocumentFragment();
+
+const createPictureListArray = () => {
+  itemPictureDescriptions.forEach((picture) => {
+    pictureListFragment.append(createItemPicture(picture));
+  });
+  return pictureListFragment;
+};
 
 pictureList.append(createPictureListArray());
--- js/picture.js
+++ js/picture.js
@@ -1,19 +1,12 @@
-import {createPosts} from './data.js';
-
 const pictureTemplate = document.querySelector('#picture').content;
 const newItemPicture = pictureTemplate.querySelector('.picture');
-const itemPictureDescriptions = createPosts();
-const pictureListFragment = document.createDocumentFragment();
 
-const createPictureListArray = () => {
-  itemPictureDescriptions.forEach((picture) => {
-    const copyItemPicture = newItemPicture.cloneNode(true);
-    copyItemPicture.querySelector('img').src = picture.url;
-    copyItemPicture.querySelector('.picture__likes').textContent = picture.likes;
-    copyItemPicture.querySelector('.picture__comments').textContent = picture.comments.length;
-    pictureListFragment.append(copyItemPicture);
-  });
-  return pictureListFragment;
+const createItemPicture = (picture) => {
+  const copyItemPicture = newItemPicture.cloneNode(true);
+  copyItemPicture.querySelector('img').src = picture.url;
+  copyItemPicture.querySelector('.picture__likes').textContent = picture.likes;
+  copyItemPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+  return copyItemPicture;
 };
 
-export {createPictureListArray};
+export {createItemPicture};

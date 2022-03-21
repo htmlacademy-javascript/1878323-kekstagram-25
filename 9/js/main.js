16243c8fdde465f35df0bba8db1258c581bcedae
Keks <keks@htmlacademy.ru>
2022-03-21 17:37:25 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,35 @@
+import './picture-upload.js';
+import './validate.js';
+import {createPosts} from './data.js';
+import {createItemPicture} from './picture.js';
+import {openPictureModal} from './picture-big.js';
+
+const postsList = createPosts();
+const picturesList = document.querySelector('.pictures');
+const pictureListFragment = document.createDocumentFragment();
+
+/**
+ * Создание HTML структуры отображения превью картинок из массива объектов.
+ *
+ * @returns {array} — массив HTML структуры.
+ */
+const createPictureList = () => {
+  postsList.forEach((picture) => {
+    pictureListFragment.append(createItemPicture(picture));
+  });
+  return pictureListFragment;
+};
+
+/**
+ * Добавление HTML структуры отображения превью картинок из массива.
+ */
+picturesList.append(createPictureList());
+
+/**
+ * Связка превью картинок на сайте и модального окна просмотра большого формата и комментариев.
+ */
+picturesList.querySelectorAll('.picture').forEach((item, i) => {
+  item.addEventListener('click', () => {
+    openPictureModal(postsList[i]);
+  });
+});

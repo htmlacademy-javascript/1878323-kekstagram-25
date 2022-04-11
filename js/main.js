6f418414301309414ecc32de90b58a9485dc736a
koteech <nosenkoaa@mbsi.bz>
2022-04-11 16:32:53 +0300

    12.9. Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import {getData} from './fetch.js';
 import {renderPictureList} from './picture.js';
 import {renderFilteredPicture} from './filters.js';
 import {showErrorLoadingModal} from './modal-messages.js';
-
 import './picture-upload.js';
 import './validate.js';
 
